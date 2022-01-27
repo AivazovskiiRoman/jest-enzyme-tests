7c912b936da70af789902ca284dad32610cbb289
Roman Himadinov <aivazovskiiroman@gmail.com>
2022-01-27 12:10:01 -0800

    updated posts component and tests for one

---
+++ src/Posts/constants.js
@@ -0,0 +1,39 @@
+export const NEWS = [
+  {
+    author: "Roman",
+    created_at: "2021-12-01T20:12:22.811Z",
+    num_comments: 10,
+    objectID: 1,
+    title: "Jest & Enzyme",
+    points: 100,
+    url: "//some-test.url",
+  },
+  {
+    author: "Natasha",
+    created_at: "2022-01-01T20:54:12.116Z",
+    num_comments: 8,
+    objectID: 2,
+    title: "TypeScript",
+    points: 10,
+    url: "//test-ts.url",
+  },
+];
+
+export const HITS = [
+  {
+    value: 10,
+    label: 10,
+  },
+  {
+    value: 20,
+    label: 20,
+  },
+  {
+    value: 40,
+    label: 40,
+  },
+  {
+    value: 50,
+    label: 50,
+  },
+];
--- src/Posts/posts.js
+++ src/Posts/posts.js
@@ -1,43 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
+
 import Post from "../Post/post";
+import Title from "../Title/title";
+import Select from "../Select/select";
+import Input from "../Input/input";
 
-const NEWS = [
-  {
-    author: "Roman",
-    created_at: "2021-12-01T20:12:22.811Z",
-    num_comments: 10,
-    objectID: 1,
-    title: "Jest & Enzyme",
-    points: 100,
-    url: "//some-test.url",
-  },
-  {
-    author: "Natasha",
-    created_at: "2022-01-01T20:54:12.116Z",
-    num_comments: 8,
-    objectID: 2,
-    title: "TypeScript",
-    points: 10,
-    url: "//test-ts.url",
-  },
-];
+import { NEWS, HITS } from "./constants";
 
-const Posts = () => (
-  <ul className="newsList">
-    {NEWS.map(
-      ({ author, created_at, num_comments, objectID, title, points, url }) => (
-        <Post
-          key={objectID}
-          author={author}
-          created_at={created_at}
-          num_comments={num_comments}
-          title={title}
-          points={points}
-          url={url}
+class Posts extends Component {
+  state = {
+    searchQuery: "",
+    hitsPerPage: 20,
+    page: 0,
+  };
+
+  handleInputChange = ({ target: { value } }) => {
+    this.setState({
+      searchQuery: value,
+    });
+  };
+
+  handleHitsChange = ({ target: { value } }) => {
+    this.setState({
+      hitsPerPage: +value,
+      page: 0,
+    });
+  };
+
+  getSearch = ({ key }) => {
+    if (key === "Enter") {
+      this.setState({
+        page: 0,
+      });
+    }
+  };
+
+  render() {
+    const { searchQuery, hitsPerPage } = this.state;
+
+    return (
+      <div className="wrapper">
+        <Title title="News" />
+        <Select
+          options={HITS}
+          handleChange={this.handleHitsChange}
+          value={hitsPerPage}
         />
-      )
-    )}
-  </ul>
-);
+        <Input
+          onKeyPress={this.getSearch}
+          onChange={this.handleInputChange}
+          value={searchQuery}
+        />
+        <ul className="newsList">
+          {NEWS.map(
+            ({
+              author,
+              created_at,
+              num_comments,
+              objectID,
+              title,
+              points,
+              url,
+            }) => (
+              <Post
+                key={objectID}
+                author={author}
+                created_at={created_at}
+                num_comments={num_comments}
+                title={title}
+                points={points}
+                url={url}
+              />
+            )
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Posts;
